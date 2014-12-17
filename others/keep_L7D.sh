813fe22736f62b01da9782063dd80489feda128a
EricaLi <erica_li@migocorp.com>
2014-12-17 15:31:55 +0800

    Added two option for command

--- others/keep_L7D.sh
+++ others/keep_L7D.sh
@@ -1,37 +1,30 @@
 #!/bin/sh
 
-# get hdfs file creation time
-
 hdfsPath=/user
-usage="Usage: dir_diff.sh [days]"
+usage="Usage: dir_diff.sh [days] [hdfsfolder]"
 now=$(date +%s)
 
-if [ ! "$1" ]
-then
-    echo $usage
-    exit 1
-fi
+days=$1
+days=7
+hdfsfolder=$2
+hdfsfolder='tmp athena pandora'
 
-for target in $(echo tmp);
+for target in $(echo ${hdfsfolder});
 do
-    echo "${hdfsPath}/${target}"
-    hadoop fs -ls ${hdfsPath}/${target} | grep 'user' | awk '{print $8}' | while read f; do
+    hadoop fs -test -e ${hdfsPath}/${target}
+    ret=$?
+    if [ ${ret} -eq 0 ]; then
+        hadoop fs -ls ${hdfsPath}/${target} | grep 'user' | awk '{print $8}' | while read f; do
 
-        dir_date=$(hadoop fs -stat $f | awk '{print $1}')
-        difference=$((($now - $(date -d "$dir_date" +%s))/(24*60*60)))
+            dir_date=$(hadoop fs -stat $f | awk '{print $1}')
+            difference=$((($now - $(date -d "$dir_date" +%s))/(24*60*60)))
 
-        echo "Now datetime: ${now}, and this is dir_date: ${dir_date}, diffdate: ${difference}"
-
-        if [ $difference -gt $1 ]; then
-            echo "hadoop fs -rm -r ${f}"
-            echo "Successfully remove ${f} from hdfs"
-        fi
-    done
-#    ret=$?
-#    if [ ${ret} -eq 0 ]; then
-#        echo "Successfully remove ${hdfsPath}/${target} from hdfs"
-#    else
-#        echo "Terminated because the remove  failure of ${hdfsPath}/${target}"
-#        exit 4
-#    fi
+            if [ $difference -gt $1 ]; then
+                hadoop fs -rm -r ${f}
+                echo "Successfully remove ${f} from hdfs"
+            fi
+        done
+    else
+        echo "File does not exist ${hdfsPath}/${target}"
+    fi
 done
