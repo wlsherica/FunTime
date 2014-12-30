47e06e1d6192d97f670d41d3c5f567d02aec54c4
Erica Li <wlsherica@hotmail.com>
2014-12-30 10:51:58 +0800

    Update tmuxSaver.sh

--- others/tmuxSaver.sh
+++ others/tmuxSaver.sh
@@ -4,6 +4,7 @@
 #0 3,21 * * * /home/shuai/tmuxEnvSaver.sh >> /home/erica_li/mylog/cron-tmux.log 2>&1
 #@reboot /home/erica_li/tmuxEnvSaver.sh >> /home/erica_li/mylog/cron-tmux.log 2>&1
 #note: reboot means run this job after reboot
+# Reference: http://segmentfault.com/blog/galoisplusplus/1190000000457385
 
 tmuxSnapshot=/.tmux_snapshot
 tmuxEXE=/usr/local/bin/tmux
@@ -11,7 +12,8 @@ save_snap()
 {
         ${tmuxEXE} list-windows -a -F"#{session_name} #{window_name} #{pane_current_command} #{pane_current_path}" > ${tmuxSnapshot}
 }
- 
+# 列出現有tmux windows, 例如 : python2.7  (1 panes) [145x38] [layout cbfd,145x38,0,0,0] @0
+
 restore_snap()
 {
         ${tmuxEXE} start-server
@@ -27,7 +29,8 @@ restore_snap()
                 ${tmuxEXE} send-keys -t "${session_name}:${window_name}" "cd ${pane_current_path}; echo \"Hint: last time you are executing '${pane_current_command}'.\"" ENTER
         done < ${tmuxSnapshot}
 }
- 
+
+#自動判斷要做snapshot或是restore 
 ps aux|grep -w tmux|grep -v grep
 if [ $? != 0 ]
 then
